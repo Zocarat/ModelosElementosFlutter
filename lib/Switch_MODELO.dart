d76c2f99e072e99728996884b3421c72f1f79ed6
jhona <jhonata.zocarato@gmail.com>
2024-01-24 14:10:43 -0300

    Adicionado Switch_MODELO

--- lib/Switch_MODELO.dart
+++ lib/Switch_MODELO.dart
@@ -1,6 +1,24 @@
+import 'package:flutter/material.dart';
 
+class Switch_MODELO extends StatefulWidget {
+  const Switch_MODELO({Key? key}) : super(key: key);
 
+  @override
+  _Switch_MODELOState createState() => _Switch_MODELOState();
+}
 
+class _Switch_MODELOState extends State<Switch_MODELO> {
+  bool _switchValor = true;
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      onChanged: (bool valor) {
+        setState(() {
+          _switchValor = valor;
+        });
+      },
+      value: _switchValor,
+    );
+  }
+}
